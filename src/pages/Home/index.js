648ccdac0affaa25a76bc359c71ace802531b1a1
Isabel Pereira <isabelalvaro116@gmail.com>
2023-02-22 23:36:53 +0000

    adicionado codigo de erro notfound

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
 import { Link } from "react-router-dom";
-import './home.css'
+import "./home.css";
 // url da api:https://api.themoviedb.org/3/movie/now_playing?api_key=8a20226013191a81fea33c6295a8e646&language=pt-BR
 
 export default function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
     async function loadFilmes() {
       const response = await api.get("movie/now_playing", {
@@ -17,9 +19,19 @@ export default function Home() {
       });
       //console.log(response.data.results.slice(0,10))
       setFilmes(response.data.results.slice(0, 10));
+      setLoading(false)
     }
     loadFilmes();
   }, []);
+
+if(loading){
+
+  return(
+    <div className="loading">
+      <h2>Carregando filmes...</h2>
+    </div>
+    )
+}
   return (
     <div className="container">
       <div className="lista-filmes">
@@ -29,7 +41,8 @@ export default function Home() {
               <strong>{filme.title}</strong>
               <img
                 src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
-              alt={filme.title} />
+                alt={filme.title}
+              />
               <Link to={`/filme/${filme.id}`}>Acessar</Link>
             </article>
           );
