9e4c7aa809d6da1265b3bd44415c57f0c7c32d92
PedroMakengo <pedromakengomiguel@gmail.com>
2023-02-23 16:20:05 +0100

    Enviando o projeto com a solução

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 import './style.css'
-export default function Header(){
-return(
-  <div>
-   <header>
-    <Link className="logo" to='/'>PrimeFlix</Link>
-    <Link className="favoritos" to='/favoritos'>Meus filmes</Link>
-   </header>
-   
-  </div>
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <Link className="logo" to="/">
+          PrimeFlix
+        </Link>
+        <Link className="favoritos" to="/favoritos">
+          Meus filmes
+        </Link>
+      </header>
+    </div>
   )
 }
--- src/pages/Filmes/index.js
+++ src/pages/Filmes/index.js
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom"
-export default function Filmes(){
-return(
-  <div>
-    <h1>Seja Bem vindo no Filme</h1>
-    <Link to='/'>Home</Link>
-  </div>
+import { Link } from 'react-router-dom'
+export default function Filmes() {
+  return (
+    <div>
+      <h1>Seja Bem vindo no Filme</h1>
+      <Link to="/">Home</Link>
+    </div>
   )
 }
